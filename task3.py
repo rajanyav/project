14d3c45f365016035448e546f45a8078d5fd6b8f
Rajanya V <63844161+rajanyav@users.noreply.github.com>
2021-11-27 11:27:19 +0530

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,10 @@
+A = {0, 2, 4, 6, 8};
+B = {1, 2, 3, 4, 5};
+
+print("Union of A and B is",A | B)
+
+print("Intersection of A and B is",A & B)
+
+print("Difference of A and B is",A - B)
+
+print("Symmetric difference of A and B is",A ^ B)
